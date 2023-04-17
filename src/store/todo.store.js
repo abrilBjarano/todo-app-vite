1cbf6f0888732d5a15a71baf9d934936e4ba23b2
abrilxcx <abrlbjrn@gmail.com>
2023-04-17 14:37:55 -0600

    111. Metodos de Store

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -22,7 +22,48 @@ const initStore = () => {
     console.log('InitStore 🐿️');
 }
 
+const loadStore = () => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = ( description ) => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} todoId
+ */
+const toggleTodo = ( todoId ) => {
+    throw new Error('Not implemented');
+}
+
+const deleteTodo = ( todoId ) => {
+    throw new Error('Not implemented');
+}
+
+const deleteCompleted = () => {
+    throw new Error('Not implemented');
+}
+
+const setFilter = ( newFilter = Filters.All ) => {
+    throw new Error('Not implemented');
+}
+
+const getCurrentFilter = () => {
+    throw new Error('Not implemented');
+}
+
 
 export default {
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
     initStore,
+    loadStore,
+    toggleTodo,
 }

fe7174e5f4e3150c61842b6aa9f46ee935db536b
abrilxcx <abrlbjrn@gmail.com>
2023-04-18 20:42:00 -0600

    115. Validaciones y optimizaciones

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,9 +1,22 @@
 import html from './app.html?raw';
+import todoStore from '../store/todo.store';
+import { renderTodos } from './use-cases';
+
+const ElementIDs = {
+    TodoList: '.todo-list',
+}
+
 /**
  * 
  * @param {String} elementId 
  */
 export const App = ( elementId ) => {
+
+    const displayTodos = () => {
+        const todos = todoStore.getTodos( todoStore.getCurrentFilter() );
+        renderTodos( ElementIDs.TodoList, todos );
+
+    };
 
     // cuando la función App() se llama
     (()=>{
@@ -11,7 +24,7 @@ export const App = ( elementId ) => {
         const app = document.createElement('div');
         app.innerHTML = html;
         document.querySelector(elementId).append( app );
-
+        displayTodos();
     })();
 
 }

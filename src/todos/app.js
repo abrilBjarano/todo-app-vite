98d42ac601f815fd441ca8b9c285c5920b0695a7
abrilxcx <abrlbjrn@gmail.com>
2023-04-18 21:13:04 -0600

    117. Toggle todos

--- src/todos/app.js
+++ src/todos/app.js
@@ -32,6 +32,7 @@ export const App = ( elementId ) => {
     // Referencias HTML
 
     const newDescriptionInput = document.querySelector( ElementIDs.NewTodoInput );
+    const todoListUL = document.querySelector( ElementIDs.TodoList );
 
     
     // Listeners
@@ -43,6 +44,12 @@ export const App = ( elementId ) => {
        todoStore.addTodo( event.target.value );
        displayTodos();
        event.target.value = '';
+    });
+
+    todoListUL.addEventListener('click', (event) => {
+        const element = event.target.closest('[data-id]');
+        todoStore.toggleTodo( element.getAttribute('data-id') );
+        displayTodos();
     });
 
 }
